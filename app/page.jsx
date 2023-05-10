ce56d624cf646e49c0db2eb3629746168fbd9991
Abrarul Islam <61227893+MdAbrar585@users.noreply.github.com>
2023-05-10 22:46:51 +0600

    Desktop Navigation done

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,20 @@
+import Feed from "@components/Feed";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+        <p className="desc text-center">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae neque
+          sunt omnis nulla suscipit tenetur consectetur sequi tempora.
+        </p>
+        <Feed />
+      </h1>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
